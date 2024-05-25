5f12fd9d31622df14298474d5a0fb809d21893fe
Ahmed alkhatim Awadeljeed <alkhatim40@gmail.com>
2024-05-25 15:45:56 +0200

    Update Hello.js

--- src/app/Hello.js
+++ src/app/Hello.js
@@ -14,11 +14,11 @@ export default () => {
                 <span className="text-primary text-sm">{'</h1>'}</span>
             </div>
             <div className="space-y-[16px]">
-                <span className="text-primary text-sm">{'<p>'}</span>
+                <span className="text-primary text-sm inline-block">{'<p>'}</span>
                 <h2 className="text-medium ps-[16px]">
                     I help business grow by crafting amazing web experiences. If you’re looking for a developer that likes to get stuff done,
                 </h2>
-                <span className="text-primary text-sm">{'</p>'}</span>
+                <span className="text-primary text-sm inline-block">{'</p>'}</span>
                 <h3 className="text-lg text-center text-primary text-bold">let’s talk <span className="inline-flex items-center justify-center bg-gray w-[40px] h-[40px] rounded-full "><img src = "/message.png" className="inline"/></span></h3>
             </div>
         </div>
