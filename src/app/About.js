6b035ef7f15a4f02ef021e65694c7d04a3c906a2
Ahmed alkhatim Awadeljeed <alkhatim40@gmail.com>
2024-06-23 14:58:43 +0400

    Update About.js

--- src/app/About.js
+++ src/app/About.js
@@ -11,11 +11,12 @@ export default () => {
                     <div className="space-y-[16px] p-[24px] bg-gray-2 rounded-[40px]">
                         <span className="text-primary text-sm inline-block">{'<p>'}</span>
                         <h2 className="text-medium ps-[16px]">
-                            <span className="text-lg text-primary">Hello! </span><br />
-                            My name is Sinan and I specialize in web developement that utilizes HTML, CSS, JS, and REACT etc.<br />
-                            I am a highly motivated individual and eternal optimist dedicated to writing clear, concise, robust code that works. Striving to never stop learning and improving.<br />
-                            When I'm not coding, I am writing bolgs, reading, or picking up some new hands-on art project like photography.<br />
-                            I like to have my perspective and belief systems challenged so that I see the world through new eyes.
+                        <span className="text-lg text-primary">Hello! </span><br />
+                            My name is Ahmed I am a seasoned <span className="text-primary">software engineer</span> with three years of hands-on experience in web development. <br />
+                            Expertise in crafting intricate systems with a focus on sophisticated <span className="text-primary">state management </span> and <span className="text-primary">component architecture</span> 
+                            . Committed to high-quality standards and <span className="text-primary">pixel-perfect</span> design-to-code implementation. <br />
+                            Developed disciplined approaches to maintaining organized code by implementing best practices and design patterns like <span className="text-primary">MVVM </span> and <span className="text-primary"> event-driven architecture. </span>
+                            Proficient in producing reusable, scalable, and easily refactored code.
                         </h2>
                         <span className="text-primary text-sm inline-block">{'</p>'}</span>
                     </div>
