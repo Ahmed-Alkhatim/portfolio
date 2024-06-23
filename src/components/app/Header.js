dbcffaceee7dc141570e5a0b5f82326babcae8ad
Ahmed alkhatim Awadeljeed <alkhatim40@gmail.com>
2024-06-23 11:34:15 +0400

    Header tablet breakpoint responsive

--- src/components/app/Header.js
+++ src/components/app/Header.js
@@ -2,8 +2,9 @@ export default function Header() {
     
     return(
         <div className="container py-[24px] border-b-2 border-gray">
-            <h1 className="text-sm text-[#FFF] flex justify-between">
-                <div className="space-x-1">
+            <h1 className="text-medium text-[#FFF] flex justify-between items-center ">
+               
+                <div className="tablet:text-[20px] space-x-1">
                     <span className="text-primary">{'<C/>'}</span>
                     <span>Ahmed alkhatim</span>
                 </div>
@@ -11,6 +12,11 @@ export default function Header() {
                     <li className="text-primary">Home</li>
                     <li>Blogs</li>
                 </ul>
+                <div className="hidden tablet:flex space-x-[32px]">
+                    <img src="/icons/discord.svg" alt=""/>
+                    <img src="/icons/github.svg" alt=""/>
+                    <img src="/icons/instagram.svg" alt=""/>
+                </div>
             </h1>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens : {
+        'tablet': '768px',
+        // => @media (min-width: 768px) { ... }
+
+        'laptop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+
+        'desktop': '1920px',
+        // => @media (min-width: 1920px) { ... }
+    },
     extend: {
       backgroundImage: {
         "about" : "url('/about.png')",
